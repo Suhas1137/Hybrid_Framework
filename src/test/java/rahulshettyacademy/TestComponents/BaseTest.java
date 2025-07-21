a737c98dabba8e8ea6300e0899288834017b222a
Suhas 1137 <jsuhas90@gmail.com>
2025-07-21 19:18:38 +0530

    fourth commit

--- src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
+++ src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
@@ -1,15 +1,23 @@
 package rahulshettyacademy.TestComponents;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import rahulshettyacademy.pageObjects.LandingPage;
@@ -18,42 +26,61 @@ public class BaseTest {
 
 	public WebDriver driver;
 	public LandingPage landingpage;
+
 	public WebDriver initializeDriver() throws IOException {
-		
-		
-		Properties prop=new Properties();
-		FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"//src//main//java//rahulshettyacademy//resources//GlobalData.properties");
+
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
+				+ "//src//main//java//rahulshettyacademy//resources//GlobalData.properties");
 		prop.load(fis);
-		String browserName=prop.getProperty("browser");
-		
-		if(browserName.equalsIgnoreCase("chrome")) {
+		String browserName = prop.getProperty("browser");
+
+		if (browserName.equalsIgnoreCase("chrome")) {
 //			ChromeOptions options = new ChromeOptions();
 //			options.addArguments("--incognito");
 			WebDriverManager.chromedriver().setup();
-		    driver = new ChromeDriver();
-			
-		}
-		else if(browserName.equalsIgnoreCase("firefox")) {
+			driver = new ChromeDriver();
+
+		} else if (browserName.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
-		    driver=new FirefoxDriver();
+			driver = new FirefoxDriver();
 		}
-		
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
 		return driver;
 	}
+
 	
-	@BeforeMethod(alwaysRun=true)
+	public List<HashMap<String, String>> getJsonDataToMap(String filepath) throws IOException {
+
+		// read json to string
+		String jsonContent = FileUtils.readFileToString(new File(filepath),
+				StandardCharsets.UTF_8);
+
+		// convert string to hashmap need dependency (jackson databind)
+
+		ObjectMapper mapper = new ObjectMapper();
+		List<HashMap<String, String>> data = mapper.readValue(jsonContent,
+				new TypeReference<List<HashMap<String, String>>>() {
+				});
+
+		return data;
+
+	}
+
+	
+	@BeforeMethod(alwaysRun = true)
 	public LandingPage launchApplication() throws IOException {
-		driver=initializeDriver();
-		landingpage=new LandingPage(driver);
+		driver = initializeDriver();
+		landingpage = new LandingPage(driver);
 		landingpage.goTo();
 		return landingpage;
 	}
-	
-	@AfterMethod(alwaysRun=true)
+
+	@AfterMethod(alwaysRun = true)
 	public void tearDown() {
 		driver.close();
 	}
-	
+
 }
--- src/test/java/rahulshettyacademy/tests/SubmitOrderTest.java
+++ src/test/java/rahulshettyacademy/tests/SubmitOrderTest.java
@@ -1,9 +1,13 @@
 package rahulshettyacademy.tests;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -16,52 +20,66 @@ import rahulshettyacademy.pageObjects.ConfirmationPage;
 import rahulshettyacademy.pageObjects.OrderPage;
 import rahulshettyacademy.pageObjects.ProductCatalogue;
 
-public class SubmitOrderTest extends BaseTest{
+public class SubmitOrderTest extends BaseTest {
 
 	String productName = "ZARA COAT 3";
-	String countryName="india";
-	
-	@Test(dataProvider="getData",groups="Purchase")
-	public void SubmitOrder(HashMap<String,String> input) throws IOException, InterruptedException{
-		
-		ProductCatalogue productCatalogue=landingpage.loginApplication(input.get("email"), input.get("password"));
-	
-		List<WebElement>products=productCatalogue.getProductList();
+	String countryName = "india";
+
+	@Test(dataProvider = "getData", groups = "Purchase")
+	public void SubmitOrder(HashMap<String, String> input) throws IOException, InterruptedException {
+
+		ProductCatalogue productCatalogue = landingpage.loginApplication(input.get("email"), input.get("password"));
+
+		List<WebElement> products = productCatalogue.getProductList();
 		productCatalogue.addProductToCart(input.get("product"));
-		CartPage cartPage=productCatalogue.goToCartPage();
-		
-		Boolean match=cartPage.verifyProductDisplay(input.get("product"));
+		CartPage cartPage = productCatalogue.goToCartPage();
+
+		Boolean match = cartPage.verifyProductDisplay(input.get("product"));
 		Assert.assertTrue(match);
-		CheckoutPage checkoutpage=cartPage.goToCheckout();
+		CheckoutPage checkoutpage = cartPage.goToCheckout();
 		checkoutpage.selectCountry(countryName);
-		ConfirmationPage confirmationPage=checkoutpage.submitOrder();
-		String confirmMessage=confirmationPage.getConfirmationMessage();
+		ConfirmationPage confirmationPage = checkoutpage.submitOrder();
+		String confirmMessage = confirmationPage.getConfirmationMessage();
 		Assert.assertTrue(confirmMessage.equalsIgnoreCase("Thankyou for the order."));
 		System.out.println(confirmMessage);
 	}
-	
-	@Test(dependsOnMethods= {"SubmitOrder"})
-	public void OrderHistoryTest(){
-		
-		ProductCatalogue productCatalogue=landingpage.loginApplication("jsuhas822@gmail.com", "Rahulshetty@123#");
-		OrderPage orderpage=productCatalogue.goToOrdersPage();
+
+	@Test(dependsOnMethods = { "SubmitOrder" })
+	public void OrderHistoryTest() {
+
+		ProductCatalogue productCatalogue = landingpage.loginApplication("jsuhas822@gmail.com", "Rahulshetty@123#");
+		OrderPage orderpage = productCatalogue.goToOrdersPage();
 		Assert.assertTrue(orderpage.verifyOrderDisplay(productName));
-		
+
 	}
-	
+
+	public String getScreenShot(String testCaseName) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File file = new File(System.getProperty("user.dir") + "//reports//" + testCaseName + ".png");
+		FileUtils.copyFile(source, file);
+		return System.getProperty("user.dir") + "//reports//" + testCaseName + ".png";
+	}
+
 	@DataProvider
-	public Object[][] getData() {
-		HashMap<String,String> map=new HashMap<String,String>();
-		map.put("email", "jsuhas822@gmail.com");
-		map.put("password", "Rahulshetty@123#");
-		map.put("product", "ZARA COAT 3");
-		
-		HashMap<String,String> map1=new HashMap<String,String>();
-		map1.put("email", "jsuhas747@gmail.com");
-		map1.put("password", "Rahul@123#");
-		map1.put("product", "ADIDAS ORIGINAL");
-		
-		return new Object[][] {{map},{map1}};
+	public Object[][] getData() throws IOException {
+
+		List<HashMap<String, String>> data = getJsonDataToMap(
+				System.getProperty("user.dir") + "//src//test//java//rahulshettyacademy//data//PurchaseOrder.json");
+
+		return new Object[][] { { data.get(0) }, { data.get(1) } };
 	}
+
+//	@DataProvider
+//	public Object[][] getData() throws IOException {
+//		HashMap<String, String> map = new HashMap<String, String>();
+//		map.put("email", "jsuhas822@gmail.com");
+//		map.put("password", "Rahulshetty@123#");
+//		map.put("product", "ZARA COAT 3");
+//
+//		HashMap<String, String> map1 = new HashMap<String, String>();
+//		map1.put("email", "jsuhas747@gmail.com");
+//		map1.put("password", "Rahul@123#");
+//		map1.put("product", "ADIDAS ORIGINAL");
 
 }
