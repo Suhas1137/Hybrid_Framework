f99146af8129d5f8e34282b67cdd778fdfdb16d0
Suhas 1137 <jsuhas90@gmail.com>
2025-07-22 17:50:32 +0530

    sixth commit

--- src/test/java/rahulshettyacademy/TestComponents/Listeners.java
+++ src/test/java/rahulshettyacademy/TestComponents/Listeners.java
@@ -17,20 +17,25 @@ public class Listeners extends BaseTest implements ITestListener{
 	
 	ExtentTest test;
 	ExtentReports extent=ExtentReporterNG.getReportObject();
+	ThreadLocal<ExtentTest> extentTest=new ThreadLocal<ExtentTest>();  // Thread safe 
 
 	@Override
 	public void onTestStart(ITestResult result) {
 		test=extent.createTest(result.getMethod().getMethodName());
+		extentTest.set(test); // thread-local provide unique thread id to each test in starting
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
-//		test.log(Status.PASS, "Test Passed");
+//		extentTest.get().log(Status.PASS, "Test Passed");
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
+//		test.log(Status.FAIL, "Test Failed");     --> to know test is failed but below line shows error message
+		extentTest.get().fail(result.getThrowable());
 		
+		// Getting driver life using below code
 		try {
 			driver=(WebDriver) result.getTestClass().getRealClass().getField("driver")
 					.get(result.getInstance());
@@ -39,42 +44,23 @@ public class Listeners extends BaseTest implements ITestListener{
 			e.printStackTrace();
 		}
 		
-		
-		
-//		test.log(Status.FAIL, "Test Failed");     --> to know test is failed but below line shows error message
-		test.fail(result.getThrowable());
+		// taking screenshot and attach to report and sending driver as parameter 
+       //		to getscreenshot method in basetest
 		String filepath = null;
 		try {
 			filepath = getScreenShot(result.getMethod().getMethodName(),driver);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		test.addScreenCaptureFromPath(filepath, result.getMethod().getMethodName());
+		extentTest.get().addScreenCaptureFromPath(filepath, result.getMethod().getMethodName());
 		
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
-		test.log(Status.SKIP, "Test Skipped");
+		extentTest.get().log(Status.SKIP, "Test Skipped");
 	}
 
-	@Override
-	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
-	}
-
-	@Override
-	public void onTestFailedWithTimeout(ITestResult result) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onTestFailedWithTimeout(result);
-	}
-
-	@Override
-	public void onStart(ITestContext context) {
-		// TODO Auto-generated method stub
-		ITestListener.super.onStart(context);
-	}
 
 	@Override
 	public void onFinish(ITestContext context) {
